18b39af4adf10b31f94c6493ee300659e4d29d84
Tyler Hood <tyler.r.hood@gmail.com>
2019-01-15 17:52:41 -0800

    Next animal down 2/6 4 to go!

--- Zoo/Zoo/Classes/Gecko.cs
+++ Zoo/Zoo/Classes/Gecko.cs
@@ -1,10 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Zoo.Interface;
 
 namespace Zoo.Classes
 {
-    class Gecko
+    //Derived from Lizard &0 IDiet
+    public class Gecko : Lizard, IDiet
     {
+        //Properties
+        public override int HasLegs { get; set; } = 4;
+        public override string Habitat { get; set; } = "forest and high tree volumn enviroments";
+        public override bool Parent { get; set; } = false;
+
+        //Methods
+        public override string Birth()
+        {
+            return "I gestate my young";
+        }
+
+        public override bool CanSwim()
+        {
+            return true;
+        }
+
+        public override string Movement()
+        {
+            return "I wiggle my way to the finish line";
+        }
+
+        //Interface Method
+        public string Diet()
+        {
+            return "I eat small bugs";
+        }       
     }
 }
