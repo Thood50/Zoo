e81738917b984418034830830a95d0283ec9cc7c
Tyler Hood <tyler.r.hood@gmail.com>
2019-01-15 18:11:42 -0800

    Jaguar and Wolf done! 4/6 concretes done

--- Zoo/Zoo/Classes/Jaguar.cs
+++ Zoo/Zoo/Classes/Jaguar.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Zoo.Interface;
 
 namespace Zoo.Classes
 {
-    class Jaguar
+    //Derived from Feline & IDiet
+    public class Jaguar : Feline, IDiet
     {
+        //Properties
+        public override string Habitat { get; set; }
+        public override bool Parent { get; set; }
+        public override bool HasFourLegs { get; set; }
+        public override bool Domestic { get; set; }
+
+        //Methods
+        public override string Birth()
+        {
+            return "I grow them in my belly";
+        }
+
+        public override string Movement()
+        {
+            return "Im super fast, neeeeeeuuuuummmmm";
+        }
+
+        //Interface Method
+        public string Diet()
+        {
+            return "I eat whatever I can catch";
+        }
     }
 }
--- Zoo/Zoo/Classes/Wolf.cs
+++ Zoo/Zoo/Classes/Wolf.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Zoo.Interface;
 
 namespace Zoo.Classes
 {
-    class Wolf
+    //Derived from Canine & IDiet
+    public class Wolf : Canine, IDiet
     {
+        //Properties
+        public override bool MansBestFriend { get; set; }
+        public override string Habitat { get; set; }
+        public override bool Parent { get; set; }
+        public override bool HasFourLegs { get; set; }
+
+        //Methods
+        public override string Birth()
+        {
+            return "I gestate things in my belly";
+        }
+
+        public override string Movement()
+        {
+            return "Quite fast, faster then most";
+        }
+
+        //Interface method
+        public string Diet()
+        {
+            return "Whatever my pack can corner";
+        }
     }
 }
